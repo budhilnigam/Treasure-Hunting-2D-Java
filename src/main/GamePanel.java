7e153d6b86baf520a2df7e1f159edbbc19253b21
budhil <budhilnigam@gmail.com>
2024-06-28 23:43:55 +0530

    Added proper time interval of motion and fps

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -16,6 +16,8 @@ public class GamePanel extends JPanel implements Runnable{
     final int screenWidth=tileSize*maxScreenCol;
     final int screenHeight=tileSize*maxScreenRow;
 
+    int FPS = 60;    
+    
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
     int playerX=100;
@@ -34,10 +36,23 @@ public class GamePanel extends JPanel implements Runnable{
         gameThread.start();
     }
     public void run(){
+        double drawInterval = 1000000000/FPS;
+        double nextDrawTime = System.nanoTime()+drawInterval;
         while(gameThread!=null){
             //System.out.println("The game loop is running");
             update();
             repaint();
+            try{
+                double remainingTime = nextDrawTime - System.nanoTime();
+                remainingTime/=1000000;
+                if(remainingTime<0){
+                    remainingTime=0;
+                }
+                Thread.sleep((long)remainingTime);
+                nextDrawTime+=drawInterval; 
+            } catch(InterruptedException e){
+                e.printStackTrace();
+            }
         }
     }
     public void update(){
