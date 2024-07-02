b3e71d46d7e2d8cfaaf02c6321fd2fe2ebad1a5f
budhil <budhilnigam@gmail.com>
2024-07-02 23:21:12 +0530

    Removed unnecessary code #1

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -59,33 +59,6 @@ public class Player extends Entity{
         }
     }
     public void draw(Graphics2D g2){
-        
-        /*int radius[] = {118,40,90,40};
-        int nPoints = 16;
-        int[] X = new int[nPoints];
-        int[] Y = new int[nPoints];
-        int max = 16;
-        for (double current=0.0; current<nPoints; current++)
-        {
-            int i = (int) current;
-            double a = Math.cos(current*((2*Math.PI)/max))*radius[i % 4];
-            double b = Math.sin(current*((2*Math.PI)/max))*radius[i % 4];
-
-            X[i] = (int) a+x;
-            Y[i] = (int) b+y;
-        }
-
-        g2.setColor(Color.getColor("000"));
-        //g2.fillPolygon(X, Y, nPoints);
-        Polygon p = new Polygon();
-        p.npoints=3;
-        for(int i=0;i<3;i++){
-            p.xpoints[i]=x+(i-1)*gp.tileSize/2;
-            p.ypoints[i]=y+(i%2)*gp.tileSize/2;
-        }
-        g2.fillPolygon(p);*/
-        /*g2.setColor(Color.WHITE);
-        g2.fillRect(x, y, gp.tileSize, gp.tileSize);*/
         BufferedImage image =null;
         switch (direction) {
             case "up":
