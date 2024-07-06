9d37f20d5fc58682385922e3a5670f908f97b18f
budhil <budhilnigam@gmail.com>
2024-07-07 00:13:51 +0530

    made some GamePanel variables public to use.

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -15,10 +15,10 @@ public class GamePanel extends JPanel implements Runnable{
     final int scale =3;//Scale of figures
 
     public final int tileSize = originalTileSize*scale; //48 x 48 tile
-    final int maxScreenCol=16;
-    final int maxScreenRow=12;
-    final int screenWidth=tileSize*maxScreenCol;
-    final int screenHeight=tileSize*maxScreenRow;
+    public final int maxScreenCol=16;
+    public final int maxScreenRow=12;
+    public final int screenWidth=tileSize*maxScreenCol;
+    public final int screenHeight=tileSize*maxScreenRow;
 
     int FPS = 60;    
     
