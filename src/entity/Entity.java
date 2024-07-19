5a50fd4eee0d1d829f5ea1bb1a8b015ff734b7f9
budhil <budhilnigam@gmail.com>
2024-07-19 23:58:44 +0530

    renamed x,y in Entity to worldX,worldY

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -3,7 +3,7 @@ package entity;
 import java.awt.image.BufferedImage;
 
 public class Entity {
-    public int x,y;
+    public int worldX,worldY;
     public int speed;
     public BufferedImage down1,down2,down3,down4;
     public String direction;
--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -30,6 +30,15 @@ public class TileManager {
             tile[2]=new Tile();
             tile[2].image=ImageIO.read(new File("./res/tiles/water.png"));
 
+            tile[3]=new Tile();
+            tile[3].image=ImageIO.read(new File("./res/tiles/water.png"));
+
+            tile[4]=new Tile();
+            tile[4].image=ImageIO.read(new File("./res/tiles/water.png"));//tree
+
+            tile[5]=new Tile();
+            tile[5].image=ImageIO.read(new File("./res/tiles/water.png"));//sand
+
         } catch (IOException e){
             e.printStackTrace();
         }
