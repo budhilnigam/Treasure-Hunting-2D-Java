6a9ef03981051036346552aeb740a8f543888cbf
budhil <budhilnigam@gmail.com>
2024-07-20 23:59:53 +0530

    Made changes such that screen will move as we move

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -16,15 +16,20 @@ public class Player extends Entity{
     GamePanel gp;
     KeyHandler keyH;
 
+    public final int screenX;
+    public final int screenY;
     public Player(GamePanel gp, KeyHandler keyH){
         this.gp = gp;
         this.keyH = keyH;
+
+        screenX = gp.screenWidth/2 - gp.tileSize/2;
+        screenY = gp.screenHeight/2 - gp.tileSize/2;
         setDefaultValues();
         getPlayerImage();
     }
     public void setDefaultValues(){    
-        x=100;
-        y=100;
+        worldX=gp.tileSize*23;
+        worldY=gp.tileSize*21;
         speed=4;
         direction="down";
     }
@@ -46,16 +51,16 @@ public class Player extends Entity{
     public void update(){
         if(keyH.upPressed){
             direction="up";
-            y-=speed;
+            worldY-=speed;
         } else if(keyH.downPressed){
             direction="down";
-            y+=speed;
+            worldY+=speed;
         } else if(keyH.leftPressed){
             direction="left";
-            x-=speed;
+            worldX-=speed;
         } else if(keyH.rightPressed){
             direction="right";
-            x+=speed;
+            worldX+=speed;
         }
     }
     public void draw(Graphics2D g2){
@@ -74,6 +79,6 @@ public class Player extends Entity{
                 image = down4;
                 break;
         }
-        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
+        g2.drawImage(image, screenX, screenY, gp.tileSize, gp.tileSize, null);
     }
 }
