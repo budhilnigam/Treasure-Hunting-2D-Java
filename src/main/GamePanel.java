509d21fa31e4ae43c238c3a9775aedb4d7d85ebe
budhil <budhilnigam@gmail.com>
2024-07-20 23:59:19 +0530

    Added world dimensions and limits

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -20,6 +20,12 @@ public class GamePanel extends JPanel implements Runnable{
     public final int screenWidth=tileSize*maxScreenCol;
     public final int screenHeight=tileSize*maxScreenRow;
 
+    //WORLD SETTINGS
+    public final int maxWorldCol=50;
+    public final int maxWorldRow=50;
+    public final int worldWidth=tileSize*maxWorldCol;
+    public final int worldHeight=tileSize*maxWorldRow;
+
     int FPS = 60;    
     
     TileManager tileM = new TileManager(this);
