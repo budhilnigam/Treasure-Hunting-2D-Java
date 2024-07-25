68efcbf02cf6fafdff3aedd3355b3774f29dbfda
budhil <budhilnigam@gmail.com>
2024-07-25 21:07:25 +0530

    Added bufferedImage for all 8 new images

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -5,6 +5,9 @@ import java.awt.image.BufferedImage;
 public class Entity {
     public int worldX,worldY;
     public int speed;
-    public BufferedImage down1,down2,down3,down4;
+    public BufferedImage down1,down2,up1,up2,left1,left2,right1,right2;
     public String direction;
+
+    public int spriteCounter=0;
+    public int spriteNum=1;
 }
