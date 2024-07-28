854f069c10ff1d532afaca2fd645e30f8b86fcfd
budhil <budhilnigam@gmail.com>
2024-07-28 23:58:11 +0530

    Removed useless comments

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -52,54 +52,6 @@ public class TileManager {
             e.printStackTrace();
         }
     }
-    /*public void draw(Graphics2D g2){
-        //*
-        g2.drawImage(tile[1].image, 0, 0, gp.tileSize, gp.tileSize, null);
-        g2.drawImage(tile[1].image, 48, 0, gp.tileSize, gp.tileSize, null);
-        g2.drawImage(tile[1].image, 96, 0, gp.tileSize, gp.tileSize, null);
-        g2.drawImage(tile[1].image, 144, 0, gp.tileSize, gp.tileSize, null);
-        g2.drawImage(tile[1].image, 192, 0, gp.tileSize, gp.tileSize, null);
-        
-        g2.drawImage(tile[1].image, 0, 48, gp.tileSize, gp.tileSize, null);
-        g2.drawImage(tile[0].image, 48, 48, gp.tileSize, gp.tileSize, null);
-        g2.drawImage(tile[0].image, 96, 48, gp.tileSize, gp.tileSize, null);
-        g2.drawImage(tile[0].image, 144, 48, gp.tileSize, gp.tileSize, null);
-        g2.drawImage(tile[1].image, 192, 48, gp.tileSize, gp.tileSize, null);
-        
-        g2.drawImage(tile[1].image, 0, 96, gp.tileSize, gp.tileSize, null);
-        g2.drawImage(tile[0].image, 48, 96, gp.tileSize, gp.tileSize, null);
-        g2.drawImage(tile[0].image, 96, 96, gp.tileSize, gp.tileSize, null);
-        g2.drawImage(tile[0].image, 144, 96, gp.tileSize, gp.tileSize, null);
-        g2.drawImage(tile[0].image, 192, 96, gp.tileSize, gp.tileSize, null);
-
-        g2.drawImage(tile[1].image, 0, 144, gp.tileSize, gp.tileSize, null);
-        g2.drawImage(tile[0].image, 48, 144, gp.tileSize, gp.tileSize, null);
-        g2.drawImage(tile[0].image, 96, 144, gp.tileSize, gp.tileSize, null);
-        g2.drawImage(tile[0].image, 144, 144, gp.tileSize, gp.tileSize, null);
-        g2.drawImage(tile[1].image, 192, 144, gp.tileSize, gp.tileSize, null);
-
-        g2.drawImage(tile[1].image, 0, 192, gp.tileSize, gp.tileSize, null);
-        g2.drawImage(tile[2].image, 48, 192, gp.tileSize, gp.tileSize, null);
-        g2.drawImage(tile[2].image, 96, 192, gp.tileSize, gp.tileSize, null);
-        g2.drawImage(tile[2].image, 144, 192, gp.tileSize, gp.tileSize, null);
-        g2.drawImage(tile[1].image, 192, 192, gp.tileSize, gp.tileSize, null);
-        //*
-        int col=0;
-        int row=0;
-        int x=0;
-        int y=0;
-        while(col<gp.maxScreenCol && row < gp.maxScreenRow){
-            g2.drawImage(tile[1].image, x, y, gp.tileSize, gp.tileSize, null);
-            x+=gp.tileSize;
-            col++;
-            if(col==gp.maxScreenCol){
-                col=0;
-                row++;
-                x=0;
-                y+=gp.tileSize;
-            }
-        }
-    }*/
     public void loadMap(String filePath){
         try {
             FileInputStream is = new FileInputStream(filePath);
@@ -110,7 +62,7 @@ public class TileManager {
                 String line = br.readLine();
                 while(col<gp.maxScreenCol){
                     String numbers[]=line.split(" ");
-                    int num=Integer.parseInt((numbers[col]));
+                    int num=Integer.parseInt(numbers[col]);
                     mapTileNum[col][row]=num;
                     col++;
                 }
